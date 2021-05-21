84edbc532683922eee8bc0510a1872b8597efb01
Catzzz <chuneleu@gmail.com>
2021-05-20 21:28:40 -0700

    react-context-demo

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import Navbar from "./Navbar";
+import Form from "./Form";
+import PageContent from "./PageContent";
+import { ThemeContext, ThemeProvider } from "./contexts/ThemeContext";
+import {LanguageContext, LanguageProvider} from "./contexts/LanguageContext";
+
+class App extends Component {
+  render() {
+    return (
+      <ThemeProvider>
+        <PageContent>
+          <LanguageProvider>
+
+          
+          <Navbar />
+          <Form />
+          </LanguageProvider>
+        </PageContent>
+      </ThemeProvider>
+    );
+  }
+}
+
+export default App;
---
+++ src/contexts/LanguageContext.js
@@ -0,0 +1,30 @@
+import React, { createContext } from "react";
+import { Component } from "react";
+
+
+export const LanguageContext = createContext();
+
+export class LanguageProvider extends Component {
+    constructor(props){
+        super(props);
+        this.state = {language:"english"};
+        this.changeLanguage = this.changeLanguage.bind(this);
+    }
+    changeLanguage(e){
+        this.setState({language:e.target.value})
+    }
+    render(){
+        return (
+            <LanguageContext.Provider value={{...this.state, changeLanguage:this.changeLanguage}}>
+                {this.props.children}
+            </LanguageContext.Provider>
+        )
+    }
+
+}
+// Higher order component
+export const withLanguateContext = Component => props => (
+    <LanguageContext.Consumer>
+        {value => <Component languageContext ={value} {...props}/>}
+    </LanguageContext.Consumer>
+);
